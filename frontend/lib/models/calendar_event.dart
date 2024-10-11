e2b299d176bb9ccaf7e16941bc1a23aa438c7bb3
Mayank Kushwaha <mayankofficial205@gmail.com>
2024-10-12 02:59:00 +0530

    calendar basic

---
+++ frontend/lib/models/calendar_event.dart
@@ -0,0 +1,17 @@
+class CalendarEvent {
+  final String id;
+  final String title;
+  final DateTime startTime;
+  final DateTime reminderTime;
+
+  CalendarEvent({required this.id, required this.title, required this.startTime, required this.reminderTime});
+
+  factory CalendarEvent.fromJson(Map<String, dynamic> json) {
+    return CalendarEvent(
+      id: json['_id'],
+      title: json['title'],
+      startTime: DateTime.parse(json['dateTime']),
+      reminderTime: DateTime.parse(json['dateTime']).subtract(const Duration(minutes: 15)),
+    );
+  }
+}
