55dfa266a86f8b897ed8c197dbef0e9bfe2fff3b
Yatika Jena <j.yatika@iitg.ac.in>
2025-03-11 20:14:03 +0530

    made separate routes and controllers for firebase and payment

---
+++ server/modules/firebase/firebase_controller.js
@@ -0,0 +1,46 @@
+import admin from 'firebase-admin';
+import User from '../user/user.model.js';
+
+// Initialize Firebase
+try {
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+    // Ensure private key is properly formatted
+    serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+
+    admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
+    console.log(' Firebase Admin SDK initialized successfully.');
+} catch (error) {
+    console.error(' Error initializing Firebase Admin SDK:', error);
+    process.exit(1);
+}
+
+// Save FCM token
+export const saveFcmToken = async (req, res) => {
+    const { userId, fcmToken } = req.body;
+    try {
+        const user = await User.findById(userId);
+        if (user) {
+            user.fcmToken = fcmToken;
+            await user.save();
+            res.status(200).json({ message: 'FCM token saved successfully' });
+        } else {
+            res.status(404).json({ message: 'User not found' });
+        }
+    } catch (err) {
+        console.error('Error saving FCM token:', err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
+
+// Get all FCM tokens
+export const getFcmTokens = async (req, res) => {
+    try {
+        const users = await User.find({});
+        const tokens = users.map(user => user.fcmToken).filter(Boolean);
+        res.json(tokens);
+    } catch (error) {
+        console.error('Error fetching tokens:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
