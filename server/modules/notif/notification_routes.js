98875f445e7f5074e59c152dd45a2b5af5ed955e
Yatika Jena <j.yatika@iitg.ac.in>
2025-03-27 19:53:30 +0530

    removed check-reminders route

--- server/modules/notif/notification_routes.js
+++ server/modules/notif/notification_routes.js
@@ -6,16 +6,16 @@ const router = express.Router();
 // Route to set reminder
 router.post('/set-reminder', setReminder);
 
-// Route to manually trigger checking reminders
-router.get('/check-reminders', async (req, res) => {
-    try {
-        await checkRemindersAndSendNotifications();
-        res.status(200).json({ message: 'Reminders checked successfully!' });
-    } catch (error) {
-        console.error('Error checking reminders:', error);
-        res.status(500).json({ error: 'Internal server error' });
-    }
-});
+//// Route to manually trigger checking reminders
+//router.get('/check-reminders', async (req, res) => {
+//    try {
+//        await checkRemindersAndSendNotifications();
+//        res.status(200).json({ message: 'Reminders checked successfully!' });
+//    } catch (error) {
+//        console.error('Error checking reminders:', error);
+//        res.status(500).json({ error: 'Internal server error' });
+//    }
+//});
 
 
 export default router;
