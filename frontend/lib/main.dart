6f33d5ef622a0700be0c1a2c848a752e5688d6b3
Yatika Jena <j.yatika@iitg.ac.in>
2025-05-15 00:32:18 +0530

    deleted redundant files

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -3,14 +3,14 @@ import 'dart:convert';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:frontend/screens/explore_screen.dart';
 import 'package:frontend/screens/home.dart';
-import 'package:frontend/screens/home_screen.dart';
 import 'package:frontend/screens/login_screen.dart';
 import 'package:frontend/services/notification_services.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:frontend/screens/splash_screen.dart';
+
 import './constants/endpoints.dart';
 import 'firebase_options.dart';
 import 'providers/eventProvider.dart';
@@ -143,18 +143,18 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'IITGsync',
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.grey[400],
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: SplashScreen(),
-      // home: const MyHomePage(title: 'IITGsync'),
-      //home:  EventShareScreen(),
+        title: 'IITGsync',
+        theme: ThemeData(
+          scaffoldBackgroundColor: Colors.grey[400],
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: ExploreScreen()
+        // home: const MyHomePage(title: 'IITGsync'),
+        //home:  EventShareScreen(),
 
-      //home:ClubsScreen(),
-    );
+        //home:ClubsScreen(),
+        );
   }
 }
 
