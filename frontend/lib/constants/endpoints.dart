3d934b6fc4e4d7e3b0dcb4d7e2be0cb14e42ddcd
Ruchir Sharma <ruchirsharma074@gmail.com>
2025-10-03 15:23:06 +0530

    Endpoints

--- frontend/lib/constants/endpoints.dart
+++ frontend/lib/constants/endpoints.dart
@@ -4,7 +4,7 @@ class AuthConfig {
   static const String CLIENT_ID =
       String.fromEnvironment('CLIENT_ID', defaultValue: '');
   static const String serverUrl =
-      String.fromEnvironment('serverUrl', defaultValue: '');
+      String.fromEnvironment('serverUrl', defaultValue: 'http://10.0.2.2:3000');
 }
 
 //change this later
@@ -47,10 +47,10 @@ class event {
       '${AuthConfig.serverUrl}/api/events/get-all-events';
   static const getUpcomingEvents =
       '${AuthConfig.serverUrl}/api/events/get-upcoming-events';
-  static const createEvent =
-      '${AuthConfig.serverUrl}/api/events/create-event';
+  static const createEvent = '${AuthConfig.serverUrl}/api/events/create-event';
   static const createTentativeEvent =
       '${AuthConfig.serverUrl}/api/events/tentative';
+  static const rsvpToEvent = '${AuthConfig.serverUrl}/api/events/rsvp/';
 }
 
 class payment {
@@ -70,10 +70,10 @@ class UserTag {
   static const String getAvailableTags = "${AuthConfig.serverUrl}/api/tags/";
 
   static String addTag(String email, String tagId) =>
-      "${AuthConfig.serverUrl}/api/user/$email/addtag/$tagId";
+      "${AuthConfig.serverUrl}/api/user/${Uri.encodeComponent(email)}/addtag/$tagId";
 
   static String removeTag(String email, String tagId) =>
-      "${AuthConfig.serverUrl}/api/user/$email/deletetag/$tagId";
+      "${AuthConfig.serverUrl}/api/user/${Uri.encodeComponent(email)}/deletetag/$tagId";
 }
 
 class ClubTag {
