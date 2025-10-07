a7ac862c319c23c74f599a4618d050b792343289
Rishi-Gupta <guptarishiiitjee@gmail.com>
2025-10-07 23:31:01 +0530

    Update active_events_screen.dart

--- frontend/lib/screens/org_screens/active_events_screen.dart
+++ frontend/lib/screens/org_screens/active_events_screen.dart
@@ -1,50 +1,51 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:frontend/constants/colors.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:frontend/constants/colors.dart';
 import 'package:frontend/screens/org_screens/RSVPIcons.dart';
 import 'package:frontend/screens/org_screens/active_events_menu.dart';
 import 'package:frontend/screens/org_screens/rsvp_info_slider.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ActiveEventsScreen extends StatefulWidget {
-  ActiveEventsScreen({super.key});
+  const ActiveEventsScreen({super.key});
 
   @override
   State<ActiveEventsScreen> createState() => _ActiveEventsScreenState();
 }
 
 class _ActiveEventsScreenState extends State<ActiveEventsScreen> {
-  var Events = null;
+  dynamic events;
 
   @override
   void initState() {
-    INIT();
+    initialize();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: Events == null ? 2 : Events.length,
+      itemCount: events == null ? 2 : events.length,
       itemBuilder: (context, index) => _ActiveEventTile(
-        Event: Events == null ? null : Events[index],
+        event: events == null ? null : events[index],
       ),
     );
   }
 
-  void INIT() async {
+  void initialize() async {
     final pref = await SharedPreferences.getInstance();
-    final creatorID = await pref.getString('userID');
+    final creatorID = pref.getString('userID');
     final response = await http.get(Uri.parse(
-        'https://iitgcampussync.onrender.com/api/events/active-events-by-creator/${creatorID}'));
+        'https://iitgcampussync.onrender.com/api/events/active-events-by-creator/$creatorID'));
     if (response.statusCode == 200) {
       if (mounted) {
-        // Check if the widget is still in the tree
+        // Check if the widget is still in the tree before calling setState
         setState(() {
-          Events = jsonDecode(response.body);
+          events = jsonDecode(response.body);
         });
       }
     }
@@ -52,236 +53,284 @@ class _ActiveEventsScreenState extends State<ActiveEventsScreen> {
 }
 
 class _ActiveEventTile extends StatelessWidget {
-  const _ActiveEventTile({super.key, required this.Event});
+  const _ActiveEventTile({required this.event});
 
-  final Event;
+  final dynamic event;
 
   @override
   Widget build(BuildContext context) {
     return _ActiveEventsCard(
-      Event: Event,
+      event: event,
     );
   }
 }
 
 class _ActiveEventsCard extends StatefulWidget {
-  const _ActiveEventsCard({super.key, required this.Event});
+  const _ActiveEventsCard({required this.event});
 
-  final Event;
+  final dynamic event;
 
   @override
   State<_ActiveEventsCard> createState() =>
-      _ActiveEventsCardState(Event: Event);
+      _ActiveEventsCardState(event: event);
 }
 
 class _ActiveEventsCardState extends State<_ActiveEventsCard> {
-  _ActiveEventsCardState({required this.Event});
+  _ActiveEventsCardState({required this.event});
 
-  final Event;
+  final dynamic event;
+
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
       child: Container(
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(
+            borderRadius: const BorderRadius.all(
               Radius.circular(16),
             ),
             border: Border.all(color: TextColors.muted)),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ClipRRect(
-              borderRadius: const BorderRadius.only(
-                topRight: Radius.circular(16),
-                topLeft: Radius.circular(16),
-              ),
-              child: Image.network(
-                Event == null
-                    ? 'https://via.placeholder.com/400x160.png/E4E4E7/000000?text=No+Image'
-                    : Event['banner'] ??
-                        'https://via.placeholder.com/400x160.png/E4E4E7/000000?text=No+Image',
-                height: 160,
-                width: double.infinity,
-                fit: BoxFit.cover,
-                errorBuilder: (context, error, stackTrace) {
-                  return Container(
-                    height: 160,
+            Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    topRight: Radius.circular(16),
+                    topLeft: Radius.circular(16),
+                  ),
+                  child: Image.network(
+                    event == null
+                        ? 'https://via.placeholder.com/400x120.png/E4E4E7/000000?text=No+Image'
+                        : event['banner'] ??
+                            'https://via.placeholder.com/400x120.png/E4E4E7/000000?text=No+Image',
+                    height: 120,
                     width: double.infinity,
-                    color: Colors.grey[300],
-                    child: const Center(
-                      child: Icon(Icons.image_not_supported,
-                          size: 50, color: Colors.grey),
+                    fit: BoxFit.cover,
+                    errorBuilder: (context, error, stackTrace) {
+                      return Container(
+                        height: 120,
+                        width: double.infinity,
+                        color: Colors.grey[300],
+                        child: const Center(
+                          child: Icon(Icons.image_not_supported,
+                              size: 50, color: Colors.grey),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                if (event != null &&
+                    DateTime.parse(event['dateTime']).isAfter(DateTime.now()))
+                  Positioned(
+                    right: 16,
+                    top: 16,
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 7, vertical: 4),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(1000)),
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 6,
+                            width: 6,
+                            decoration: BoxDecoration(
+                                color: Colors.red,
+                                borderRadius: BorderRadius.circular(1000)),
+                          ),
+                          const SizedBox(
+                            width: 4,
+                          ),
+                          const Text(
+                            'Live',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w400, fontSize: 12),
+                          )
+                        ],
+                      ),
                     ),
-                  );
-                },
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.fromLTRB(16, 16, 16, 16),
-              alignment: Alignment.centerLeft,
-              child: Text(
-                Event == null ? 'Test Event' : Event['title'] ?? "Test Event",
-                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              alignment: Alignment.centerLeft,
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.watch_later_outlined,
-                    size: 18,
                   ),
-                  SizedBox(
-                    width: 2,
+              ],
+            ),
+            Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.fromLTRB(16, 16, 16, 8),
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    event == null
+                        ? 'Test Title'
+                        : event['title'] ?? "Test Title",
+                    style: const TextStyle(
+                        fontWeight: FontWeight.w600, fontSize: 20),
                   ),
-                  Text(
-                    Event == null
-                        ? 'Test DateTime'
-                        : DateFormat('d MMMM, h:mm a').format(
-                                    DateTime.parse(Event['dateTime'])) ==
-                                ''
-                            ? "Test DateTime"
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  alignment: Alignment.centerLeft,
+                  child: Row(
+                    children: [
+                      const Icon(
+                        Icons.watch_later_outlined,
+                        size: 18,
+                      ),
+                      const SizedBox(
+                        width: 2,
+                      ),
+                      Text(
+                        event == null
+                            ? 'Test dateTime'
                             : DateFormat('d MMMM, h:mm a')
-                                .format(DateTime.parse(Event['dateTime'])),
-                    style: TextStyle(
-                      fontWeight: FontWeight.w400,
-                      fontSize: 14,
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 17.5, vertical: 8),
-              alignment: Alignment.centerLeft,
-              child: Row(
-                children: [
-                  SvgPicture.asset(
-                    'assets/icons/locationIcon.svg',
-                    height: 16.33,
-                    width: 13.67,
+                                .format(DateTime.parse(event['dateTime'])),
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 14,
+                        ),
+                      )
+                    ],
                   ),
-                  SizedBox(
-                    width: 4,
+                ),
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 17.5, vertical: 4),
+                  alignment: Alignment.centerLeft,
+                  child: Row(
+                    children: [
+                      SvgPicture.asset(
+                        'assets/icons/locationIcon.svg',
+                        height: 16.33,
+                        width: 13.67,
+                      ),
+                      const SizedBox(
+                        width: 4,
+                      ),
+                      Text(
+                        event == null
+                            ? 'Test Venue'
+                            : event['venue'] ?? "Test Venue",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 14,
+                        ),
+                      )
+                    ],
                   ),
-                  Text(
-                    Event == null
-                        ? 'Test Venue'
-                        : Event['venue'] ?? "Test Venue",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w400,
-                      fontSize: 14,
-                    ),
-                  )
-                ],
-              ),
+                ),
+              ],
             ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 RSVPIcons(
-                  RSVP: Event == null ? [] : Event['RSVP'] ?? [],
+                  // RSVP: event == null ? [] : event['RSVP'] ?? [],
+                  RSVP: const [''], // Using placeholder data
                 ),
-                Text(
-                  '${Event == null ? '0' : Event['views'] ?? '0'} views',
-                  style: TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.w400,
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text(
+                    '${event == null ? '0' : event['views'] ?? '0'} Views',
+                    style: const TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w400,
+                    ),
                   ),
                 )
               ],
             ),
             Divider(
-              height: 2,
+              height: 1,
               color: TextColors.muted,
             ),
             Row(
               children: [
-                const SizedBox(height: 72),
-                const SizedBox(width: 16),
-                SizedBox(
-                  width: 161,
-                  height: 40,
-                  child: ElevatedButton(
-                    onPressed: () {
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 16, 4, 16),
+                  child: InkWell(
+                    onTap: () {
                       // TODO: implement See Insights
                     },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFF09090B),
-                      elevation: 0,
-                      padding: EdgeInsets.zero,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(100.0),
+                    borderRadius: BorderRadius.circular(999),
+                    child: Container(
+                      height: 40,
+                      width: (screenWidth - 112) * 161 / 248,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(999),
                       ),
-                    ),
-                    child: const Text(
-                      'See Insight',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 14,
-                          fontWeight: FontWeight.w600),
+                      child: const Text(
+                        'See Insight',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600),
+                      ),
                     ),
                   ),
                 ),
-                const SizedBox(width: 4),
-                SizedBox(
-                  width: 87,
-                  height: 40,
-                  child: ElevatedButton(
-                    onPressed: () {
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 16, 4, 16),
+                  child: InkWell(
+                    onTap: () {
                       // TODO: implement Edit
                     },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFFE4E4E7),
-                      elevation: 0,
-                      padding: EdgeInsets.zero,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(100.0),
+                    borderRadius: BorderRadius.circular(999),
+                    child: Container(
+                      height: 40,
+                      width: (screenWidth - 112) * 87 / 248,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFE4E4E7),
+                        borderRadius: BorderRadius.circular(999),
                       ),
-                    ),
-                    child: const Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.mode_edit_outlined,
-                          color: Colors.black,
-                          size: 16,
-                        ),
-                        SizedBox(width: 6),
-                        Text(
-                          'Edit',
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 14,
-                              fontWeight: FontWeight.w600),
-                        ),
-                      ],
+                      child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.mode_edit_outlined,
+                            color: Colors.black,
+                            size: 18,
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(8.0, 0, 0, 0),
+                            child: Text(
+                              'Edit',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-                const SizedBox(width: 4),
-                Container(
-                  width: 40,
-                  height: 40,
-                  decoration: const BoxDecoration(
-                    color: Color(0xFFE4E4E7),
-                    shape: BoxShape.circle,
-                  ),
-                  child: Material(
-                    color: Colors.transparent,
-                    child: InkWell(
-                      borderRadius: BorderRadius.circular(20),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 16, 0, 16),
+                  child: Column(children: [
+                    InkWell(
+                      borderRadius: BorderRadius.circular(999),
                       onTap: () {
-                        showActiveEventsMenu(context, Event);
+                        showActiveEventsMenu(context, event);
                       },
-                      child: const Icon(
-                        Icons.more_horiz,
-                        color: Colors.black,
+                      child: Container(
+                        padding: const EdgeInsets.all(7),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(999),
+                          color: const Color(0xFFE4E4E7),
+                        ),
+                        child: const Icon(
+                          Icons.more_horiz,
+                          color: Colors.black,
+                        ),
                       ),
                     ),
-                  ),
+                  ]),
                 ),
                 const SizedBox(width: 16),
               ],
